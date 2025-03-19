fb8a11e071c4ad851fc5cf287fc5fde98638e0fa
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-19 10:28:27 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 11/Swap2Numbers_in_python.py
@@ -0,0 +1,14 @@
+# Let's Start here
+# Input two numbers
+a=int(input('Enter 1 number : '))
+b=int(input('Enter 2 number : '))
+print('Before Swapping')
+print('A = ',a)
+print('B = ',b)
+a=a+b
+b=a-b   # Swapping Logic
+a=a-b
+# Final Result
+print('After Swapping')
+print('A = ',a)
+print('B = ',b)
