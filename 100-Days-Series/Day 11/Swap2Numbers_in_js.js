93bf57cb43647ddc3317cacfbaa86d007df42a79
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-19 10:27:44 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 11/Swap2Numbers_in_js.js
@@ -0,0 +1,10 @@
+// Let's Start here
+let a = 10; // A=10
+let b = 20; // B=20
+
+console.log("Before swapping: \na = "+ a+ "\nb = "+b);
+a = a + b;
+b = a - b;  // Swapping Logic
+a = a - b;
+// Final Result
+console.log("After swapping: \na = "+ a+ "\nb = "+b);
