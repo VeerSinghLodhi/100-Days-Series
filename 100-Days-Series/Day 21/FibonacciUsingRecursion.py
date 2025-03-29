ea82e18275d63c8834f8871583cca9fe58dc4e9f
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-29 23:54:48 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 21/FibonacciUsingRecursion.py
@@ -0,0 +1,14 @@
+
+# Recursive method to calculate Fibonacci numbers
+def getFibonacci(num):
+    if num<=1:
+        return num
+    else:
+        return getFibonacci(num-1)+getFibonacci(num-2)
+    
+# Let's Start here
+#// Number of terms in the Fibonacci series
+num=int(input("Enter series terms : "))
+print("Fibonacci series up to ",num,"term")
+for i in range(0,num,1):
+    print(getFibonacci(i),end=" ")
