acc44d427f8f2d1ae61701ec1f1d877b9e402789
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-03-29 23:58:10 +0530

    Update FibonacciUsingRecursion.py

--- 100-Days-Series/Day 21/FibonacciUsingRecursion.py
+++ 100-Days-Series/Day 21/FibonacciUsingRecursion.py
@@ -4,7 +4,7 @@ def getFibonacci(num):
     if num<=1:
         return num   # Base cases: Fibonacci(0) = 0, Fibonacci(1) = 1
     else:
-        return getFibonacci(num-1)+getFibonacci(num-2)
+        return getFibonacci(num-1)+getFibonacci(num-2) # Recursive call
     
 # Let's Start here
 #// Number of terms in the Fibonacci series
