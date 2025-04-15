39aea33370a67df4dfca414fef818729e0943344
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-04-15 22:34:26 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 38/IntersectionOfTwoArrays.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// Functiont to Find common elements between two arrays.
+void findCommonElements(int arr1[],int size1,int arr2[],int size2) {
+    cout<<"\nCommon elements: ";
+    for (int i = 0; i < size1; i++) {
+        for (int j = 0; j < size2; j++) {
+            if (arr1[i] == arr2[j]) {
+                cout<<arr1[i]<<" ";
+                break; // Avoid duplicates
+            }
+        }
+    }
+    cout<<endl;
+}
+// Let's Start here
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {3, 4, 5, 6, 7};
+    int size1=sizeof(arr1)/sizeof(arr1[0]);
+    int size2=sizeof(arr2)/sizeof(arr2[0]);
+    findCommonElements(arr1,size1,arr2,size2);;
+}
