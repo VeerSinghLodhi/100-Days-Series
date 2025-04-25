665dda9154cc2a4dc7e4c0c0593d3655c23ba8c5
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-04-25 22:12:01 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 48/LinkedListDS.java
@@ -0,0 +1,62 @@
+class LinkedList{
+    int data;
+    LinkedList next;
+    public LinkedList(int data) {
+        this.data=data;
+        this.next=null;
+    }
+    
+}
+public class LinkedListDS {
+    private static  LinkedList head=null;
+    public static java.util.Scanner scanner=new java.util.Scanner(System.in);
+    public static void addNode(){
+        System.out.print("Enter data : ");
+        int data=scanner.nextInt();
+        LinkedList newNode=new LinkedList(data);
+        if(head==null){
+            head=newNode;
+            newNode.next=null;
+        }else{
+            LinkedList p,last=head;
+            for(p=head;p!=null;p=p.next){
+                last=p;
+            }
+            last.next=newNode;
+            newNode.next=null;
+        }
+        System.out.println("New Node Added");
+    }
+    public static void traverse(){
+        if(head==null){
+            System.out.println("Linked List is empty!");
+        }else{
+            LinkedList temp;
+            for(temp=head;temp!=null;temp=temp.next){
+                System.out.println("Data is "+temp.data);
+                System.out.println("---------------------------------");
+            }
+        }
+    }
+    public static void main(String [] args){
+        int ch;
+        do{
+            System.out.println("1. Add New Node");
+            System.out.println("2. Traverse");
+            System.out.println("3. Exit");
+            System.out.print("Select a choice : ");
+            ch=scanner.nextInt();
+            if(ch==1){
+                addNode();
+            }else if(ch==2){
+                traverse();
+            }else if(ch==3){
+                System.out.println("Exited");
+            }else{
+                System.err.println("Invalid choice");
+            }
+
+        }while(ch!=3);
+    }
+
+}
