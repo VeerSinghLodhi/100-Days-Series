bc5bbf0a45a5e3d964aafaf4d6227aae8c7324df
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-04-26 22:59:52 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 49/LinkedListDS.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct LinkedList{
+    int data;
+    struct LinkedList *next;   
+}*head;
+void addNode(){
+    struct LinkedList *ptr;
+    ptr=(struct LinkedList*)malloc(sizeof(struct LinkedList));
+    if(ptr==NULL){
+        printf("\nCouldn't allocated memory!");
+    }else{
+        printf("\nEnter data : ");
+        scanf("%d",&ptr->data);
+        if(head==NULL){
+            head=ptr;
+            head->next=NULL;
+        }else{
+            struct LinkedList *p,*last;
+            for(p=head;p!=NULL;p=p->next){
+                last=p;
+            }
+            last->next=ptr;
+            ptr->next=NULL;
+        }
+        printf("\nNew Node Added");
+    }
+}
+void traverse(){
+    if(head==NULL){
+        printf("\nNo Noded Found!");
+    }else{
+        struct LinkedList *temp;
+        for(temp=head;(temp);temp=temp->next){
+            printf("\nData is %d",temp->data);
+            printf("\n---------------------------");
+        }
+    }
+}
+void delNode(){
+    if(head==NULL){
+        printf("\nLinked List is empty!!");
+    }else{
+        int no,res=0;
+        printf("\nEnter data to delete : ");
+        scanf("%d",&no);
+        struct LinkedList *temp,*prev;
+        for(temp=head;(temp);temp=temp->next){
+            if(temp->data==no){
+                if(temp==head){
+                    head=head->next;
+                }else if(temp->next==NULL){
+                    prev->next=NULL;
+                }else{
+                    prev->next=temp->next;
+                }
+                free(temp);
+                printf("\nData Deleted");
+                res=1;
+                break;
+            }
+            prev=temp;
+        }
+        if(!res){
+            printf("\nData is not found!");
+        }
+    }
+}
+// Let' Start here
+void main(){
+    int ch;
+    do{
+        printf("\n1. Add New Node");
+        printf("\n2. Traverse");
+        printf("\n3. Delete");
+        printf("\n4. Exit");
+        printf("\nSelect a choice : ");
+        scanf("%d",&ch);
+        if(ch==1){
+            addNode();
+        }else if(ch==2){
+            traverse();
+        }else if(ch==3){
+            delNode();
+        }else if(ch==4){
+            printf("\nExited");
+        }else{
+            printf("\nInvalid choice");
+        }
+    }while(ch!=4);
+}
