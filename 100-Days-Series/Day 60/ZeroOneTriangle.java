aecf79c7cf75629cdcb7a39648d55a8fd84e532d
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-05-07 23:18:03 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 60/ZeroOneTriangle.java
@@ -0,0 +1,23 @@
+public class ZeroOneTriangle {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+        for (int i = 1; i <= n; i++) {
+            int start;
+            if (i % 2 == 0) {
+                start = 0; // Even row starts with 0
+            } else {
+                start = 1; // Odd row starts with 1
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(start + " ");
+                if (start == 1) {
+                    start = 0;
+                } else {
+                    start = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
