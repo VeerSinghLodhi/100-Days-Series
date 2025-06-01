cf9e4687ce4a45ebaedafbb3c8c623f9f054495e
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-01 22:19:47 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 85/SumOfSquares.py
@@ -0,0 +1,10 @@
+
+# Let's Start Here
+# Input : value of n
+n=int(input('Enter the value of n : '))
+
+# Formula of calculate the sum of square series
+sum=(n*(n+1)*(2*n+1))/6
+
+# Output : the sum of seires
+print('Sum of series',sum)
