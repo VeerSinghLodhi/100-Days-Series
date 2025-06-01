4fc1091e8f8ed0e6d3bed832550510ccb761ee03
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-01 22:19:29 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 85/SumOfSquares.js
@@ -0,0 +1,10 @@
+
+// Let's Start Here
+//value of n
+let n=5;
+
+// Formula of calculate the sum of squares 
+let sum = ( n * ( n + 1 ) * ( 2 * n + 1 ) ) / 6;
+
+// Output : the sum of series
+console.log("Sum of Series : "+sum);
