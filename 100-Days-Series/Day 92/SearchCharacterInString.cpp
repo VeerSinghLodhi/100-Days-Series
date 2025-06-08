510255e26ea8fd21f429cda35e3b600841b50ece
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-08 23:59:20 +0530

    Add files via upload

---
+++ 100-Days-Series/Day 92/SearchCharacterInString.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// Function to find character in a string
+int getFindCharacter(string str,char ch){
+
+    for(int i=0;i<str.length();i++){
+        if(str[i]==ch){
+            return i;
+        }
+    }
+    return -1;
+}
+// Let's Start Here
+int main(){
+    string str;
+    cout<<"\nEnter a string : ";
+    getLine(cin,str);
+    cout<<"\nEnter Character to find : ";
+    char ch;
+    cin>>ch;
+    int res=getFindCharacter(str,ch);
+    if(res!=-1){
+        cout<<"\nFound at "<<res<<endl;
+    }else{
+        cout<<"\nNot Found"<<endl;
+    }
+}
