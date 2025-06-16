424e2f3a9abbd5bb065d67d60e03944da81d490f
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-17 00:25:31 +0530

    Update GraphRepresentationUsingAdjacencyList.java

--- 100-Days-Series/Day 100/GraphRepresentationUsingAdjacencyList.java
+++ 100-Days-Series/Day 100/GraphRepresentationUsingAdjacencyList.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
 class Node {
     int data;
 
@@ -9,10 +8,8 @@ class Node {
         this.data = data;
     }
 }
-
 class Graph {
     private List<List<Node>> adjList;
-
     // Constructor to initialize the graph with `v` vertices
     Graph(int v) {
         adjList = new ArrayList<>();
@@ -20,13 +17,11 @@ class Graph {
             adjList.add(new LinkedList<>());
         }
     }
-
     // Method to add an undirected edge between two vertices
     void addEdge(int u, int v) {
         adjList.get(u).add(new Node(v));
         adjList.get(v).add(new Node(u));
     }
-
     // Method to print the adjacency list of the graph
     void printGraph() {
         for (int v = 0; v < adjList.size(); v++) {
@@ -38,12 +33,11 @@ class Graph {
         System.out.println();
     }
 }
-
 public class GraphRepresentationUsingAdjacencyList {
+    // Main Method
     public static void main(String[] args) {
         int v = 5; // Number of vertices in the graph
         Graph graph = new Graph(v);
-
         // Add edges
         graph.addEdge(0, 1); // Edge between 1 and 2
         graph.addEdge(0, 2); // Edge between 1 and 3
@@ -51,7 +45,6 @@ public class GraphRepresentationUsingAdjacencyList {
         graph.addEdge(2, 3); // Edge between 3 and 4
         graph.addEdge(2, 4); // Edge between 3 and 5
         graph.addEdge(3, 4); // Edge between 4 and 5
-
         // Print the adjacency list
         graph.printGraph();
     }
